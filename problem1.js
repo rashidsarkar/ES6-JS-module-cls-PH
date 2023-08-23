c694e30c1410f4f0645914ceeb5357d28c19221b
rashidsarkar <rashidrock558@gmail.com>
2023-08-23 14:17:40 +0600

    problem 2

---
+++ problem1.js
@@ -0,0 +1,9 @@
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+console.log(multiply(2, 3, 4));
+const string = `i am a web Developer.
+I love to code . 
+i love to eat birany
+`;
+console.log(string);
+const sum = (num1, num2 = 0) => num1 + num2;
+console.log(sum(4, 5));
---
+++ problem2.js
@@ -0,0 +1,21 @@
+const frindsWithJor = (arr) => {
+  let newArr = [];
+  for (name of friends) {
+    if (name.length % 2 == 0) {
+      newArr.push(name);
+    }
+  }
+  return newArr;
+};
+const friends = [
+  "sabbir",
+  "karim,",
+  "rahim",
+  "nabila",
+  "toma",
+  "salma",
+  "himu",
+];
+console.log(frindsWithJor(...friends));
+
+const myMathWork = (arr) => {};
